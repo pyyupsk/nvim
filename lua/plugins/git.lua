a36556d7eed5d38aad14078425454ea459859a2a
Pongsakorn Thipayanate <pyyupsk@proton.me>
2026-05-04 17:19:46 +0700

    feat(git): add octo.nvim for GitHub issues and PRs

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -23,4 +23,27 @@ return {
       { "<leader>gH", "<cmd>DiffviewFileHistory<cr>", desc = "Branch history" },
     },
   },
+
+  {
+    "pwntester/octo.nvim",
+    cmd = "Octo",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+      "echasnovski/mini.icons",
+    },
+    keys = {
+      { "<leader>gi", "<cmd>Octo issue list<cr>", desc = "List issues" },
+      { "<leader>gI", "<cmd>Octo issue create<cr>", desc = "Create issue" },
+      { "<leader>gp", "<cmd>Octo pr list<cr>", desc = "List PRs" },
+      { "<leader>gP", "<cmd>Octo pr create<cr>", desc = "Create PR" },
+      { "<leader>gr", "<cmd>Octo review start<cr>", desc = "Start PR review" },
+      { "<leader>gS", "<cmd>Octo search<cr>", desc = "Search GitHub" },
+    },
+    opts = {
+      enable_builtin = true,
+      picker = "telescope",
+      use_local_fs = true,
+    },
+  },
 }
